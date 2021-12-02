54e7d5cafd7bc54ed1d3aa6bae7bf959e829b028
cbm888 <cbmarch88@gmail.com>
2021-12-02 17:03:25 -0600

    Stopwatch that works kinda

---
+++ Timerz/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace Timerz {
+    class Program {
+        static void Main(string[] args) {
+            Watch watch = new Watch();
+            watch.StartTime();
+            watch.StopTime();
+        }
+    }
+}
---
+++ Timerz/Watch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace Timerz {
+    public class Watch {
+
+        private Stopwatch stopWatch;
+        private string userInput;
+
+        public void StartTime() {
+            Begin:
+            Console.WriteLine("Type 'Start' to begin the timer.");
+            userInput = Console.ReadLine();
+            if (userInput  == "Start") 
+            stopWatch = Stopwatch.StartNew();
+            else {
+                Console.WriteLine("Please type 'Start' in order to begin the timer.");
+                goto Begin;
+            }
+        }
+        public void StopTime() {
+            
+            Console.WriteLine("Press any key followed by enter to stop the stopwatch. Type 5 + enter to restart.");
+            userInput = Console.ReadLine();
+            stopWatch.Stop();
+            Console.WriteLine("The Time elapsed was" + stopWatch.Elapsed);
+            if (userInput == "5") {
+                stopWatch.Reset();
+                stopWatch.Restart();
+                StartTime();
+                StopTime();
+            }
+        }
+
+
+    }
+}
